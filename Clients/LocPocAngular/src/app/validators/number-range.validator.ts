e2567c59dee9d3d597105df8acdcbb77f3be95eb
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2020-01-30 20:39:12 +0100

    Fixed lat/long range validation after the input type has been changed to number

--- Clients/LocPocAngular/src/app/validators/number-range.validator.ts
+++ Clients/LocPocAngular/src/app/validators/number-range.validator.ts
@@ -26,19 +26,21 @@ export class NumberRangeValidator implements Validator {
   }
 
   validate(control: AbstractControl): ValidationErrors {
-    const value: string = control.value;
-    if (value !== null) {
-      const numValue = Number(value);
-      if (Number.isNaN(numValue) || (typeof value === 'string' && value.length === 0)) {
+    if (!control.value) {
+      return {
+        incorrectRange: true
+      };
+    }
+    const numValue = Number(control.value);
+    if (Number.isNaN(numValue)) {
+      return {
+        incorrectRange: true
+      };
+    } else {
+      if (numValue > this.range.max || numValue < this.range.min) {
         return {
           incorrectRange: true
         };
-      } else {
-        if (numValue > this.range.max || numValue < this.range.min) {
-          return {
-            incorrectRange: true
-          };
-        }
       }
     }
     return null;
