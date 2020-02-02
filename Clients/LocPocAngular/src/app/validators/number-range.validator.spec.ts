ef886d0d6e13403d0730a75737589b3ccb497110
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2020-02-02 10:54:53 +0100

    Added tests for NumberRangeValidator

---
+++ Clients/LocPocAngular/src/app/validators/number-range.validator.spec.ts
@@ -0,0 +1,44 @@
+import { NumberRangeValidator, NumberRange } from './number-range.validator';
+import { AbstractControl } from '@angular/forms';
+
+describe('NumberRangeValidator', () => {
+    let validator: NumberRangeValidator;
+
+    beforeEach((() => {
+        validator = new NumberRangeValidator();
+    }));
+
+    it('should detect incorrect range when value is a number', () => {
+        const range: NumberRange = {
+            min: -10,
+            max: 10
+        };
+
+        validator.validationContext = range;
+        let control = { value: 11 };
+        expect(validator.validate(control as AbstractControl).incorrectRange).toBeTruthy();
+        control = { value: -11 };
+        expect(validator.validate(control as AbstractControl).incorrectRange).toBeTruthy();
+        control = { value: 0 };
+        expect(validator.validate(control as AbstractControl)).toBeNull();
+        control = { value: 10 };
+        expect(validator.validate(control as AbstractControl)).toBeNull();
+        control = { value: -10 };
+        expect(validator.validate(control as AbstractControl)).toBeNull();
+        const control2 = { value: '-10' };
+        expect(validator.validate(control2 as AbstractControl)).toBeNull();
+    });
+
+    it('should detect incorrect range when value is not a number', () => {
+        const range: NumberRange = {
+            min: -10,
+            max: 10
+        };
+
+        validator.validationContext = range;
+        let control = { value: 'ABC' };
+        expect(validator.validate(control as AbstractControl).incorrectRange).toBeTruthy();
+        control = { value: null };
+        expect(validator.validate(control as AbstractControl).incorrectRange).toBeTruthy();
+    });
+});
